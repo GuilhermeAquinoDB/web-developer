885638ffcc1e59931ca8b6ecb2784cd7d9bfd36c
GuilhermeAquinoDB <Guilhermevl@hotmail.com>
2021-03-23 16:40:28 -0300

    Array

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,39 @@
+// Aula01
+
 // Variaveis
-var nome = "Guilherme Aquino";
-var n1 = 5;
-var n2 = 3;
-var frase = "Japão é o melhor time do mundo!";
+//var nome = "Guilherme Aquino";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo!";
 //alert(nome + " tem "+ idade + " anos");
 //alert(idade + idade2);
 
-console.log(nome);
-//console.log(idade + idade2);
+/*console.log(nome);
+console.log(idade + idade2);
 console.log(n1 * n2);
 console.log(n1 - n2);
 console.log(n1 / n2);
 console.log(n1 ^ n2);
 console.log(frase.replace("Japão", "Brasil"));
-//console.log(frase.toUpperCase());
-//console.log(frase.toLowerCase());
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());*/
+
+// Aula02
+
+//Array
+var lista = ["maça", "pera","laranja"];
+
+//Dicionário
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}]
+console.log(frutas);
+console.log(frutas[1].nome);
+
+console.log(lista);
+console.log(lista[1]);
+
+//lista.push("uva");
+//lista.pop();
+console.log(lista.length);
+//console.log(lista.reverse());
+console.log(lista.toString());
+//console.log(lista.join(" | "));
